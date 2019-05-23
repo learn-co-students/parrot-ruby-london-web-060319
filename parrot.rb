a904afe742c05bcba0a2c3b0be562ea3d58e6793
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-23 21:00:07 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
+require 'pry'
+
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(phrase="Squawk!")
+  if phrase == "Squawk!"
+    puts "Squawk!"
+  else
+    puts phrase
+  end
+  return phrase
+end
